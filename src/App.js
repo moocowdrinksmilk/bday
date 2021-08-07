5dcfb404453050998def210a1b65f3906fbb3bcd
Rui Xian <tayrx9988@gmail.com>
2021-08-08 01:49:54 +0800

    add lotties

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Lottie from 'react-lottie';
+import * as cat from './assets/56906-cat-in-box.json'
+import * as confetti from './assets/confetti.json'
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: cat.default,
+  };
+  const defaultOptionsCon = {
+    loop: true,
+    autoplay: true,
+    animationData: confetti.default,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen bg-red-100 flex items-center justify-center relative">
+      <div className="absolute top-0 right-0 bottom-0 left-0 z-10">
+      <Lottie
+            
+            options={defaultOptionsCon} />
+      </div>
+      <div>
+        <div className="flex">
+          <Lottie
+            height={400}
+            width={400}
+            options={defaultOptionsCon} />
+          <Lottie options={defaultOptions}
+            height={400}
+            width={400} />
+          <Lottie
+            height={400}
+            width={400}
+            options={defaultOptionsCon} />
+        </div>
+      </div>
     </div>
   );
 }
